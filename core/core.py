348cf45a2be4385ae91f02c9937617e1517329a6
Genemator Sakhib (Sokhibjon Orzikulov) <sakhib.orzklv@gmail.com>
2020-04-23 21:21:21 +0500

    Adding logging support

--- core/core.py
+++ core/core.py
@@ -1,10 +1,12 @@
 import telebot
 import config
+import logging
 
+logger = telebot.logger
 bot = telebot.TeleBot(config.TOKEN)
 
 
 def launch():
-    print("Bot has been started!")
+    telebot.logger.setLevel(logging.DEBUG)
     bot.polling(none_stop=True)
     pass
